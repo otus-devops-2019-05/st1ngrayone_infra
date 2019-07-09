56b5487430a429241bdd18bf8b32474ae0f4c46f
Anton Cherenkov <zay-chirik@ya.ru>
2019-07-09 13:02:14 +0300

    multiple ssh-keys added to metadata default

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,6 +11,12 @@ provider "google" {
 	region = "${var.region}"
 }
 
+resource "google_compute_project_metadata" "default" {
+        metadata = {
+        ssh-keys = "root:${file(var.public_key_path)}\nappuser1:${file(var.public_key_path)}"
+        }
+}
+
 resource "google_compute_instance" "app" {
 	name = "reddit-app"
 	machine_type = "g1-small"
@@ -47,6 +53,7 @@ resource "google_compute_instance" "app" {
 	metadata {
 		# путь до публичного ключа
 		ssh-keys = "root:${file(var.public_key_path)}"
+		
 	}
 
 }
